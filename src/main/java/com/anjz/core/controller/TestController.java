fecaaaf2a6ec65685180037147f301bd8f2e001f
shuai·ding <1114332905@qq.com>
2017-03-17 22:32:19 +0800

    sping配置完善

--- src/main/java/com/anjz/core/controller/TestController.java
+++ src/main/java/com/anjz/core/controller/TestController.java
@@ -4,7 +4,9 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.anjz.core.model.SysAuth;
 import com.anjz.core.service.intf.test.TestService;
 
 @Controller
@@ -25,6 +27,21 @@ public class TestController {
 	public String testException(){
 		testService.testException();		
 		return "/test/uploadFile";
+	}
+	
+	
+	@RequestMapping("/ajax1")
+	@ResponseBody
+	public String ajax1(){
+		return "aaa";
+	}
+	
+	@RequestMapping("/ajax2")
+	@ResponseBody
+	public SysAuth ajax2(){
+		SysAuth sysAuth = new SysAuth();
+		return sysAuth;
+		
 	}
 
 }
