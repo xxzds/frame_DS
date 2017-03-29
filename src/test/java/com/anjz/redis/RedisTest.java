5ea40f81e80ac6cdf14894a591c6c073d5e7b4c3
shuai·ding <1114332905@qq.com>
2017-03-29 20:27:52 +0800

    redis测试

--- src/test/java/com/anjz/redis/RedisTest.java
+++ src/test/java/com/anjz/redis/RedisTest.java
@@ -1,6 +1,7 @@
 package com.anjz.redis;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.Resource;
@@ -14,9 +15,12 @@ import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.anjz.BaseTest;
+import com.anjz.result.BaseResult;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 /**
+ * 注意StringRedisSerializer和JdkSerializationRedisSerializer的区别
  * @author ding.shuai
  * @date 2016年9月27日上午1:30:06
  */
@@ -57,4 +61,39 @@ public class RedisTest extends BaseTest{
 		});
 	}
 
+	/**
+	 * hash 操作
+	 */
+	@Test
+	public void mapTest(){
+		redisTemplate.execute(new RedisCallback<Boolean>() {
+
+			@Override
+			public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+				
+				StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+				JdkSerializationRedisSerializer jdkSerializationRedisSerializer = new JdkSerializationRedisSerializer();
+				
+				byte[] key =stringRedisSerializer.serialize("test");
+				Map<byte[],byte[]> hashes =Maps.newHashMap();
+				hashes.put(stringRedisSerializer.serialize("name"), jdkSerializationRedisSerializer.serialize("丁帅"));
+				hashes.put(stringRedisSerializer.serialize("object"), jdkSerializationRedisSerializer.serialize(new BaseResult()));
+				logger.info("********************************start********************************");
+				//创建
+				connection.hMSet(key, hashes);
+				
+				//查询
+//				List<byte[]> lists =  connection.hMGet(key,stringRedisSerializer.serialize("name"));
+				Map<byte[],byte[]> hashesResult = connection.hGetAll(key);
+				for(byte[] key1:hashesResult.keySet()){					
+					logger.info("key="+stringRedisSerializer.deserialize(key1)+",value="+jdkSerializationRedisSerializer.deserialize(hashesResult.get(key1)));
+				}
+				
+				//删除
+				connection.del(key);
+				logger.info("********************************end********************************");
+				return null;	
+			}
+		});
+	}
 }
